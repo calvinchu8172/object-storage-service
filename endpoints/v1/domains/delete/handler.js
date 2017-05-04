0dfe80538bee28f1b7e944ff375d3ea0fa2a79b3
Calvin Chu <calvinchu@ecoworkinc.com>
2017-05-04 10:59:48 +0800

    [#23719] 13 Delete Domain API-開發
    * 開發
    
    ref #23719

---
+++ endpoints/v1/domains/delete/handler.js
@@ -0,0 +1,30 @@
+'use strict';
+
+require( 'rootpath' )();
+const AWS             = require('aws-sdk');
+const SERVICE         = process.env.SERVERLESS_PROJECT;
+const STAGE           = process.env.SERVERLESS_STAGE;
+const REGION          = process.env.SERVERLESS_REGION;
+const ddb             = new AWS.DynamoDB.DocumentClient({ region: REGION });
+const paramsFetcher   = require('lib/params_fetcher.js');
+const CommonSteps     = require('lib/common_steps');
+
+module.exports.handler = ( event, context, callback ) => {
+
+  console.log("************aaaaa");
+
+  const headers = event.headers;
+  const receivedParams = paramsFetcher.fetchFrom(event);
+  // const a = 'aaa'
+
+  console.log(event);
+  console.log("************");
+  console.log(headers);
+  console.log("************");
+  console.log(receivedParams);
+  console.log("************");
+
+  const response = { code: '0000', message: 'OK' };
+  callback(null, response);
+
+};
