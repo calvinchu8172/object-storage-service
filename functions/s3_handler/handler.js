c18b41308af2656540c0781e1f0e949c68d808ef
Calvin Chu <calvinchu@ecoworkinc.com>
2017-05-18 16:27:23 +0800

    [#23807] 15 House Keeping-開發
    * 開發
    
    ref #23807

--- functions/s3_handler/handler.js
+++ functions/s3_handler/handler.js
@@ -23,8 +23,6 @@ module.exports.handler = ( event, context, callback ) => {
 
   const s3_record = event.Records;
 
-  
-
   CommonSteps.parseS3Record(s3_record)
     .then((promises) => {
       return CommonSteps.updateObjectUsage(promises);
